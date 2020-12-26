e88457a1c4980f1ccfc58cb1be3027c3dd57466b
Nikita Yankovskii <yannikita98@gmail.com>
2020-12-27 00:51:20 +0300

    added identity matrix constructor and fix move constr

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include "src/Matrix.h"
 
 int main() {
-    int n = 2;
-    int m = 2;
+    uint32_t n = 2;
+    uint32_t m = 2;
     Matrix a(n, m);
     Matrix b(n, m);
+    Matrix d = Matrix::identity(n);
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             std::cin >> a[i][j];
